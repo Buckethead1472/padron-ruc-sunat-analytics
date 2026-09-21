fdecf1b3096358a6b793520d0a6e6f4bcd668d7e
Yamil Nair Solis Diaz <Oasisblack12@gmail.com>
2026-09-20 21:29:21 -0500

    Update 01_mortalidad_departamento.sql

--- sql/01_mortalidad_departamento.sql
+++ sql/01_mortalidad_departamento.sql
@@ -1,11 +1,9 @@
 -- Pregunta de negocio: ¿Qué departamentos tienen mayor riesgo de mortalidad empresarial?
--- Insight completo: ver /insights/01_mortalidad_departamento.md
+-- Insight completo: ver insights/01_mortalidad_departamento.md
 -- Stakeholder: Analista de riesgo crediticio
 
-
-
 WITH datos_base AS (
-    SELECT 
+    SELECT
         u.departamento,
         COUNT(*) FILTER (WHERE pr.estado_contribuyente LIKE 'BAJA%') AS baja,
         COUNT(*) FILTER (WHERE pr.estado_contribuyente NOT LIKE 'BAJA%') AS activo,
@@ -20,7 +18,7 @@ promedio_ponde AS (
     FROM datos_base
 ),
 rank_tasa AS (
-    SELECT 
+    SELECT
         departamento,
         tasa,
         baja,
@@ -28,13 +26,14 @@ rank_tasa AS (
         ROW_NUMBER() OVER (ORDER BY tasa DESC) AS rankeo
     FROM datos_base
 )
-SELECT 
+SELECT
     departamento,
-	tasa,
-	baja,
-	activo,
-	promedio_ponderado,
-	rankeo,
-	CASE WHEN tasa > promedio_ponderado THEN 'Por encima del promedio' ELSE 'Por debajo del promedio' END AS clasificacion
+    tasa,
+    baja,
+    activo,
+    promedio_ponderado,
+    rankeo,
+    CASE WHEN tasa > promedio_ponderado THEN 'Por encima del promedio' ELSE 'Por debajo del promedio' END AS clasificacion
 FROM rank_tasa
-CROSS JOIN promedio_ponde;
+CROSS JOIN promedio_ponde
+ORDER BY rankeo;
